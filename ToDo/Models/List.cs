faf5c0c186a9b7303dcedce40605de324172e326
andrewhoon <hoonandrew1709@gmail.com>
2017-02-08 11:51:40 -0500

    added models

---
+++ ToDo/Models/List.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDo.Models
+{
+    public class List
+    {
+        [Key]
+        public int ListID { get; set; }
+        public string Title { get; set; }
+        public DateTime Date { get; set; }
+
+        public virtual ICollection<Task> Tasks { get; set; }
+
+
+    }
+}
